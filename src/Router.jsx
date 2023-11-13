e18ecc2728e673bddc742f3e1fc17b7f6c90c400
jlg777 <proyetojlg777@gmail.com>
2023-11-13 07:18:48 -0300

    login y regiter pages

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import { HomePages } from './pages/HomePages';
 import { TasksPage } from './pages/TasksPage';
+import { UserPage } from './pages/UserPage';
 
 export function Router () {
     return (
         <Routes>
           <Route path="/" element={<HomePages />} />
           <Route path="/tareas" element={<TasksPage />} />
+          <Route path="/login" element={<UserPage />} />
         </Routes>
     )
 }
---
+++ src/pages/UserPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Register from '../components/Register'
+import Login from '../components/Login'
+
+export const UserPage = () => {
+  return (
+    <div>
+        UserPage
+        <Register />
+        <Login />
+    </div>
+    
+  )
+}
+ 
